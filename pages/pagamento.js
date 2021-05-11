ca0b5d53eb01eccd2c9b834dc42d09fc9cb31987
Luiza Campos <luiza.almcampos@gmail.com>
2021-05-11 08:34:34 -0300

    lodash fixed

--- pages/pagamento.js
+++ pages/pagamento.js
@@ -59,34 +59,7 @@ const ConfirmButton = styled(Button)`
 
 export default function Pagamento() {
 
-    // const cards = {
-    //     Visa: /^4[0-9]{12}(?:[0-9]{3})/,
-    //     Mastercard: /^5[1-5][0-9]{14}/,
-    //     Amex: /^3[47][0-9]{13}/,
-    //     DinersClub: /^3(?:0[0-5]|[68][0-9])[0-9]{11}/,
-    //     Discover: /^6(?:011|5[0-9]{2})[0-9]{12}/,
-    //     JCB: /^(?:2131|1800|35\d{3})\d{11}/
-    // };
-    
-    // function testarCC(nr, cards) {
-    //     for (let card in cards) {
-    //         if (nr.match(card)) {
-    //             return true
-    //         }
-    //             return false
-    //     }
-    // }
-
-    // function getNumberCard(e) {
-    //     const number = e.target.value
-    //     console.log(number)
-    //     testarCC(number, cards)
-    // }
-
-    // function confirm(e) {
-    //     e.preventDefault()
-    //     alert('Pagamento realizado com sucesso!')
-    // }
+   
 
     return (
         <>
